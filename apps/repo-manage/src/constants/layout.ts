3e6dd9ef6004c02e5089b83f9ae43c89ea45199a
Bert van Beek <d.a.v.beek@tue.nl>
2025-12-09 21:13:44 +0100

    chore: extract OUTPUT_NEAR_BOTTOM_PX constant
    Move magic number for scroll detection threshold to constants.

--- apps/repo-manage/src/constants/layout.ts
+++ apps/repo-manage/src/constants/layout.ts
@@ -4,3 +4,4 @@ export const RESIZE_DEBOUNCE_MS = 300
 export const TAB_MIN_WIDTH = 400
 export const CONSOLE_MIN_HEIGHT = 120
 export const SUCCESS_FLASH_MS = 500
+export const OUTPUT_NEAR_BOTTOM_PX = 8
