0e46f4ab5e4a9d6e52962801944e30aeb4154bd4
Bert van Beek <d.a.v.beek@tue.nl>
2026-01-05 22:56:53 +0100

    fix: add text-foreground to TabsContent for proper theming

--- packages/ui/src/components/ui/tabs.tsx
+++ packages/ui/src/components/ui/tabs.tsx
@@ -71,7 +71,7 @@ function TabsContent({
   return (
     <TabsPrimitive.Content
       data-slot="tabs-content"
-      className={cn("flex-1 outline-none", className)}
+      className={cn("flex-1 text-foreground outline-none", className)}
       {...props}
     />
   )
